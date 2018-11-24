33a44712776482e28cee2d7ad15476b7a034d596
2Garin <surru@mail.ru>
2018-11-25 00:37:45 +0500

    Create core.js

---
+++ app/public/js/core.js
@@ -0,0 +1,46 @@
+function register_user(){
+	$.post("/register", {username:$('#r_username').val(), password: $('#r_password').val()},function(data){
+		data=JSON.parse(data);
+		if(data.ans){
+			$('#r_username').val('');
+			$('#r_password').val('');
+			$('#a_status').html('Пользователь успешно зарегистрирован, можете авторизоваться');
+		}else{
+			$('#a_status').html('Ошибка, пользователь с таким именем уже существует');
+		};
+	});	
+};
+
+function auth_local(){
+	$.post("/auth", {username:$('#l_username').val(), password: $('#l_password').val()},function(data){
+		data=JSON.parse(data);
+		if(data.ans){
+			$('#logon_forms').css('display','none');
+			$('#content').html('Добрый день, '+data.username);
+		};
+	});
+};
+
+function getusers(){
+	$.get('/getdata', {act:'getusers'}, function(data) {
+		$('#content').html(data);
+	});	
+};
+
+function getusers(){
+	$.get('/getdata', {act:'getcontacts'}, function(data) {
+		$('#content').html(data);
+	});	
+};
+
+function getfullusers(){
+	$.get('/getdata', {act:'getfullusers'}, function(data) {
+		$('#content').html(data);
+	});	
+};
+
+function getpartners(){
+	$.get('/getdata', {act:'getpartners'}, function(data) {
+		$('#content').html(data);
+	});	
+};
